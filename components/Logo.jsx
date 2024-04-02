7fe8ebd15d448869c9a5f5ab8cc495e78b6e1f1e
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2024-04-03 00:33:46 +0600

    logo components create

---
+++ components/Logo.jsx
@@ -0,0 +1,19 @@
+import logo from "@assets/lws_logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+    return (
+        <>
+            <Link href="/">
+                <Image
+                    class="max-w-[100px] md:max-w-[165px]"
+                    src={logo}
+                    alt="Lws"
+                />
+            </Link>
+        </>
+    );
+};
+
+export default Logo;
